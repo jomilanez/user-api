5a6edb9b1fff9808184a63f2a13c90587b2344e1
jmilanez <josiane.milanez@gmail.com>
2019-04-12 07:16:59 +0200

    Simple version

--- src/main/kotlin/com/jomilanez/userapi/SwaggerProperties.kt
+++
@@ -1,6 +0,0 @@
-package com.jomilanez.contentapi
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties("swagger")
-class SwaggerProperties(var host: String = "", var protocol: String = "")
