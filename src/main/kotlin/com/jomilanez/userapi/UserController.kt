f6ceb75f1d6587e5070bcb349c538d0403d20107
jmilanez <josiane.milanez@gmail.com>
2019-04-12 07:59:43 +0200

    Adding swagger

--- src/main/kotlin/com/jomilanez/userapi/UserController.kt
+++ src/main/kotlin/com/jomilanez/userapi/UserController.kt
@@ -1,5 +1,6 @@
 package com.jomilanez.userapi
 
+import io.swagger.annotations.*
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/user")
+@Api(description = "Users management API", tags = ["UserDto"])
 class UserController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Gets a user")
+    @ApiResponses(
+        ApiResponse(code = 201, message = "UserDto has been found"),
+        ApiResponse(code = 500, message = "Internal server error")
+    )
     fun getUser(): UserDto = UserDto(userId = "id", country = "BR", language = "pt")
 }
 
