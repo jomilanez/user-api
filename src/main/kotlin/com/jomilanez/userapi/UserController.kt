9f7db89f96806aa9332663298bdca34de29f47c9
jmilanez <josiane.milanez@gmail.com>
2019-04-12 07:16:59 +0200

    Simple version

--- src/main/kotlin/com/jomilanez/userapi/UserController.kt
+++ src/main/kotlin/com/jomilanez/userapi/UserController.kt
@@ -1,27 +1,18 @@
 package com.jomilanez.userapi
 
-import io.swagger.annotations.*
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/user")
-@Api(description = "Users management API", tags = ["UserDto"])
 class UserController {
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation("Registers new user")
-    @ApiResponses(
-        ApiResponse(code = 201, message = "UserDto has been successfully created"),
-        ApiResponse(code = 500, message = "Internal server error")
-    )
-    fun create(
-        @ApiParam(value = "Application Version", required = true)
-        @RequestHeader(name = "X-App-Version", required = true) appVersion: String,
-        @ApiParam(value = "UserDto object containing all the information needed to create a user", required = true)
-        @RequestBody user: UserDto
-    ): UserDto = UserDto(userId = "id", country = "BR", language = "pt")
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun getUser(): UserDto = UserDto(userId = "id", country = "BR", language = "pt")
 }
 
 data class UserDto(
