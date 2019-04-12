74fb0db51642ea327b57982a71ce11e57d4df25b
jmilanez <josiane.milanez@gmail.com>
2019-04-12 08:10:14 +0200

    Adding api key

--- src/main/kotlin/com/jomilanez/userapi/SwaggerConfiguration.kt
+++ src/main/kotlin/com/jomilanez/userapi/SwaggerConfiguration.kt
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiInfo
-import springfox.documentation.service.Contact
+import springfox.documentation.service.*
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+const val API_KEY_HEADER = "api-key"
 
 @EnableSwagger2
 @Configuration
@@ -24,6 +26,9 @@ class SwaggerConfiguration(private val swaggerProperties: SwaggerProperties) {
             .host(swaggerProperties.host)
             .protocols(setOf(swaggerProperties.protocol))
             .apiInfo(apiInfo())
+            .securitySchemes(listOf(apiKeyWithName(API_KEY_HEADER)))
+            .securityContexts(listOf(SecurityContext.builder().securityReferences(globalAuth()).build()))
+
             .useDefaultResponseMessages(false)
             .produces(setOf("application/json"))
             .select()
@@ -42,4 +47,15 @@ class SwaggerConfiguration(private val swaggerProperties: SwaggerProperties) {
             .build()
 
     }
+
+    private fun globalAuth(): List<SecurityReference> = listOf(
+        SecurityReference(
+            API_KEY_HEADER, listOf(
+                AuthorizationScope("*", "all access to resources")
+            ).toTypedArray()
+        )
+    )
+
+    private fun apiKeyWithName(name: String): ApiKey = ApiKey(name, name, "header")
+
 }
